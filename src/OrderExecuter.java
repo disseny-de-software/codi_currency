8d4d1982c00fb679f346dd684723bbfc7bc7c164
joan-s <joans@cvc.uab.es>
2022-11-20 19:19:46 +0100

    afegeixo missatges logging

--- src/OrderExecuter.java
+++ src/OrderExecuter.java
@@ -4,17 +4,23 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class OrderExecuter {
   private List<Order> orders = new ArrayList<>();
   private Timer timer = new Timer();
   private final long delay = 0L;
   private final long period = 2000L; // milliseconds
+  static Logger logger = LoggerFactory.getLogger(OrderExecuter.class);
 
   private TimerTask repeatedTask = new TimerTask() {
     public void run() {
       LocalDateTime now = LocalDateTime.now();
       //System.out.println("Task performed on " + now);
-      System.out.print(".");
+      //System.out.print(".");
+      logger.debug("Task performed on " + now);
+
       List<Order> ordersToRemove = new ArrayList<>();
       // better do not edit the list during traversal
 
@@ -23,12 +29,16 @@ public class OrderExecuter {
           order.execute();
           if (order.isDone()) {
             ordersToRemove.add(order);
-            System.out.println("Order " + order + " to be removed because it has completed");
+            String message = "Order " + order + " to be removed because it has completed";
+            //System.out.println(message);
+            logger.info(message);
           }
         } else {
           ordersToRemove.add(order);
-          System.out.println("Order " + order + " to be removed because it's past its deadline "
-                  + order.getDateTime());
+          String message = "Order " + order + " to be removed because it's past its deadline "
+                  + order.getDateTime();
+          //System.out.println(message);
+          logger.info(message);
         }
       }
       for (Order order : ordersToRemove) {
@@ -37,9 +47,7 @@ public class OrderExecuter {
     }
   };
 
-  public OrderExecuter() {
-    timer.scheduleAtFixedRate(repeatedTask, delay, period);
-  }
+  public OrderExecuter() { timer.scheduleAtFixedRate(repeatedTask, delay, period); }
 
   public void placeOrder(Order order) {
     if (order.getDateTime().isAfter(LocalDateTime.now())) {
